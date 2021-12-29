73e8c0eebe629c179c529124344c4be2cecae12a
Levno_710 <elioelsch@oelschner.eu>
2021-12-29 17:11:51 +0100

    Added minify preset

---
+++ src/configs.lua
@@ -0,0 +1,14 @@
+-- This Script is Part of the Prometheus Obfuscator by Levno_710
+--
+-- pipeline.lua
+--
+-- This Script Provides some configuration presets
+
+return {
+    ["Minify"] = {
+        LuaVersion = "Lua51";
+        Steps = {
+
+        };
+    }
+}

2d8a7fc5c5e67f64db8121043bc64da13c537325
Levno_710 <elioelsch@oelschner.eu>
2022-01-03 16:42:45 +0100

    Added new literal generation

--- src/obfuscator/randomLiterals.lua
+++ src/obfuscator/randomLiterals.lua
@@ -7,20 +7,33 @@ local RandomStrings = require("obfuscator.randomStrings");
 
 local RandomLiterals = {};
 
-function RandomLiterals.String()
-    return RandomStrings.randomStringNode();
+local function callNameGenerator(generatorFunction, ...)
+	if(type(generatorFunction) == "table") then
+		generatorFunction = generatorFunction.generateName;
+	end
+	return generatorFunction(...);
+end
+
+function RandomLiterals.String(pipeline)
+    return Ast.StringExpression(callNameGenerator(pipeline.namegenerator, math.random(1, 4096)));
+end
+
+function RandomLiterals.Dictionary()
+    return RandomStrings.randomStringNode(true);
 end
 
 function RandomLiterals.Number()
     return Ast.NumberExpression(math.random(-8388608, 8388607));
 end
 
-function RandomLiterals.Any()
-    local type = math.random(1, 2);
+function RandomLiterals.Any(pipeline)
+    local type = math.random(1, 3);
     if type == 1 then
-        return RandomLiterals.String();
+        return RandomLiterals.String(pipeline);
     elseif type == 2 then
         return RandomLiterals.Number();
+    elseif type == 3 then
+        return RandomLiterals.Dictionary();
     end
 end
 

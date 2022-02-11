75cf374d782f02e488ed5ecf75a4eeb3e27b00b0
Levno_710 <elioelsch@oelschner.eu>
2022-02-11 16:01:26 +0100

    Updated Pipeline

--- src/prometheus/pipeline.lua
+++ src/prometheus/pipeline.lua
@@ -201,7 +201,7 @@ function Pipeline:apply(code, filename)
 	
 	logger:info(string.format("Generated Code size is %.2f%% of the Source Code size", (string.len(code) / sourceLen)*100))
 	
-	code = "--[[\n	" .. config.Watermark .. "\n]]\n" .. code;
+	code = "--[[\n    " .. config.Watermark .. "\n]]\n" .. code;
 	
 	return code;
 end

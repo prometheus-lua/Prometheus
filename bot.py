cb69c649a9f029cb8a8f95b12a91feb254fa00e5
James <42681518+jmesfo0@users.noreply.github.com>
2022-10-09 02:43:21 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,64 @@
+from importlib.resources import path
+import discord
+from discord.ext import commands
+import requests
+import os
+from os import system
+import subprocess
+import shutil
+import string
+import random
+
+token = "token." #token
+channel_id = 1021868744525488310 #channel id
+
+file_path = os.path.abspath(os.path.dirname(__file__))
+
+bot = commands.Bot(command_prefix="!")
+bot.remove_command("help")
+letters = string.ascii_uppercase
+filename = ''.join(random.choice(letters) for i in range(7))
+
+def obfuscation(file):
+    subprocess.check_output(f'lua ./cli.lua --preset Medium {file}')
+    os.remove(file)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online ✔️")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="prometheus lua obfuscator"))
+
+@bot.event
+async def on_message(message):
+    channel = str(message.channel)
+    author=str(message.author)
+    channel = bot.get_channel(channel_id)
+
+    try:
+        url = message.attachments[0].url
+        if not message.author.bot:
+            if message.channel.id == channel_id:
+                if message.attachments[0].url:
+                    if '.lua' not in url:
+                        embed=discord.Embed(title=f"***Wrong file extension!***", description=f"only ``.lua`` allowed", color=0xFF3357)
+                        message = await channel.send(embed=embed)
+                    else:
+                        obfuscated_dir = f"{file_path}\\obfuscated\\"
+
+                        if not os.path.exists(obfuscated_dir):
+                            os.makedirs(obfuscated_dir)
+                            
+                        print(f'\nNew lua script received from {author}.')
+                        print(f'Attachment Link: {message.attachments[0].url}\n')
+                        response = requests.get(url)
+                        path = f"{file_path}\\obfuscated\\{filename}.lua"
+
+                        open(path, "wb").write(response.content)
+                        obfuscation(path)
+                        embed=discord.Embed(title="File has been obfuscated", color=0x3357FF)
+                        await channel.send(embed=embed, file=discord.File(f"{file_path}\\obfuscated\\{filename}.obfuscated.lua"))
+                        os.remove(f"{file_path}\\obfuscated\\{filename}.obfuscated.lua")
+    except:
+        pass
+
+bot.run(token)

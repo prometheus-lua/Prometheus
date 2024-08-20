c99016d2a2abd3237220c28bfa973f9342b5807f
fartylocal <125145031+fartylocal@users.noreply.github.com>
2024-08-20 18:37:28 +0200

    Update namegenerators.lua

--- src/prometheus/namegenerators.lua
+++ src/prometheus/namegenerators.lua
@@ -4,4 +4,5 @@ return {
 	Il = require("prometheus.namegenerators.Il");
 	Number = require("prometheus.namegenerators.number");
 	Confuse = require("prometheus.namegenerators.confuse");
+	Underscore = require("prometheus.namegenerators.underscore");
 }

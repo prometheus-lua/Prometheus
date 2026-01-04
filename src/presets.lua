b1cc5d790ebb6fe4f39600b241420b88c115cc1a
--get <chamianramesesmendez@gmail.com>
2026-01-04 10:29:49 +0800

    Correct repeat statement compilation in Vmify, and add comprehensive test suite for repeat statement

--- src/presets.lua
+++ src/presets.lua
@@ -37,7 +37,7 @@ return {
             {
                 Name = "Vmify";
                 Settings = {
-                    
+
                 };
             },
             {
@@ -52,6 +52,27 @@ return {
                 Settings = {
 
                 }
+            },
+        }
+    };
+    ["Vmify"] = {
+        -- The default LuaVersion is Lua51
+        LuaVersion = "Lua51";
+        -- For minifying no VarNamePrefix is applied
+        VarNamePrefix = "";
+        -- Name Generator for Variables that look like this: IlI1lI1l
+        NameGenerator = "MangledShuffled";
+        -- No pretty printing
+        PrettyPrint = false;
+        -- Seed is generated based on current time
+        Seed = 0;
+        -- Obfuscation steps
+        Steps = {
+            {
+                Name = "Vmify";
+                Settings = {
+
+                };
             },
         }
     };
@@ -83,7 +104,7 @@ return {
             {
                 Name = "Vmify";
                 Settings = {
-                    
+
                 };
             },
             {
@@ -126,7 +147,7 @@ return {
             {
                 Name = "Vmify";
                 Settings = {
-                    
+
                 };
             },
             {
@@ -144,7 +165,7 @@ return {
             {
                 Name = "Vmify";
                 Settings = {
-                    
+
                 };
             },
             {

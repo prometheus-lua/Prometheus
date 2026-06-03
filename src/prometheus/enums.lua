18119d87ec7cb43e75d729699f9816e8ded17675
kendratiphanie-hash <kendratiphanie@gmail.com>
2026-06-03 10:14:26 +0800

    Add full Roblox LuaU support with type annotations and Roblox-specific keywords

--- src/prometheus/enums.lua
+++ src/prometheus/enums.lua
@@ -11,6 +11,7 @@ local chararray = require("prometheus.util").chararray;
 Enums.LuaVersion = {
 	LuaU = "LuaU" ,
 	Lua51 = "Lua51",
+	RobloxLuaU = "RobloxLuaU",
 }
 
 Enums.Conventions = {
@@ -74,6 +75,54 @@ Enums.Conventions = {
 			"(", ")", "{", "}", "[", "]",
 			";", ":", ",", ".", "..", "...",
 			"::", "->", "?", "|", "&",
+		},
+
+		IdentChars = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"),
+		NumberChars = chararray("0123456789"),
+		HexNumberChars = chararray("0123456789abcdefABCDEF"),
+		BinaryNumberChars = {"0", "1"},
+		DecimalExponent = {"e", "E"},
+		HexadecimalNums = {"x", "X"},
+		BinaryNums = {"b", "B"},
+		DecimalSeperators = {"_"},
+
+		EscapeSequences = {
+			["a"] = "\a";
+			["b"] = "\b";
+			["f"] = "\f";
+			["n"] = "\n";
+			["r"] = "\r";
+			["t"] = "\t";
+			["v"] = "\v";
+			["\\"] = "\\";
+			["\""] = "\"";
+			["\'"] = "\'";
+		},
+		NumericalEscapes = true,
+		EscapeZIgnoreNextWhitespace = true,
+		HexEscapes = true,
+		UnicodeEscapes = true,
+	},
+	[Enums.LuaVersion.RobloxLuaU] = {
+		Keywords = {
+			"and", "break", "do", "else", "elseif", "continue",
+			"end", "false", "for", "function", "if",
+			"in", "local", "nil", "not", "or",
+			"repeat", "return", "then", "true", "until", "while",
+			-- Roblox LuaU type annotation keywords
+			"type", "class", "interface", "export", "import",
+		},
+
+		SymbolChars = chararray("+-*/%^#=~<>(){}[];:,.@"),
+		MaxSymbolLength = 4,
+		Symbols = {
+			"+", "-", "*", "/", "%", "^", "#",
+			"==", "~=", "<=", ">=", "<", ">", "=",
+			"+=", "-=", "/=", "%=", "^=", "..=", "*=",
+			"(", ")", "{", "}", "[", "]",
+			";", ":", ",", ".", "..", "...",
+			"::", "->", "?", "|", "&", "<>",
+			"<:", "@", "??", "??=",
 		},
 
 		IdentChars = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"),
